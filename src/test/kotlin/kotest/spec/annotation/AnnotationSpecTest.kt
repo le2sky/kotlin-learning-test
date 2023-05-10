c0428a7ee4787586c77015612407e6ce67e085d9
leesky <leehaneul0623@gmail.com>
2023-05-11 04:56:08 +0900

    test : annotation spec test

---
+++ src/test/kotlin/kotest/spec/annotation/AnnotationSpecTest.kt
@@ -0,0 +1,34 @@
+package kotest.spec.annotation
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+internal class AnnotationSpecTest : AnnotationSpec() {
+
+    private val sut = Calculator()
+
+    @Test
+    fun `1과 2를 더하면 3이다`() {
+        val actual = sut.plus(1, 2)
+        actual shouldBe 3
+    }
+
+    @Test
+    fun `1과 2를 곱하면 2다`() {
+        val actual = sut.mult(1, 2);
+        actual shouldBe 2
+    }
+
+    @Test
+    fun `입력값이 null이면 예외를 발생한다`() {
+        shouldThrow<IllegalArgumentException> {
+            sut.mult(1, null);
+        }
+    }
+}
+
+class Calculator {
+    fun plus(a: Int, b: Int) = a + b;
+    fun mult(a: Int, b: Int?) = a * (b ?: throw IllegalArgumentException());
+}
