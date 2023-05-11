f4eaf521d00f824304b00d31bc8eb853e0735cae
leesky <leehaneul0623@gmail.com>
2023-05-11 20:21:55 +0900

    test : vararg test

--- src/test/kotlin/function/FunctionTest.kt
+++ src/test/kotlin/function/FunctionTest.kt
@@ -3,7 +3,6 @@ package function
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
 
-
 internal class FunctionTest : AnnotationSpec() {
     @Test
     fun `default parameter`() {
@@ -21,6 +20,18 @@ internal class FunctionTest : AnnotationSpec() {
     fun `Unit은 반환값이 없을때 사용`() {
         val unit = unitFun()
         unit shouldBe Unit
+    }
+
+    @Test
+    fun `vararg는 여러 파라미터를 받을 수 있다`() {
+        val allMessages = getAllMessages("temp", "le2sky", "hello", prefix = "test")
+        val allMessages2 = getAllMessages(*allMessages, prefix = "test")
+        allMessages shouldBe allMessages2
+        allMessages.size shouldBe 3
+    }
+
+    private fun getAllMessages(vararg messages: String, prefix: String): Array<out String> {
+        return messages
     }
 
     private fun unitFun() {
